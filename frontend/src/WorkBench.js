e79b6183c5f5832171159273e487ed7fc886a4ca
MartinNose <liucmartin@qq.com>
2021-03-01 10:05:18 +0800

    some update

---
+++ frontend/src/WorkBench.js
@@ -0,0 +1,41 @@
+const { Component } = require("react");
+const env = require('./.env.js')
+const url = env.url;
+
+
+class WorkBench extends Component {
+
+    componentDidMount() {
+        
+    }
+
+    constructor() {
+        super()
+    
+        this.state = {
+          doctor: {
+            id: "",
+            name: "",
+            department: "",
+            education: "",
+            email: "",
+            experience: "",
+            gender: "",
+            avatarURL: "",
+            status: "",
+            score: ""
+          },
+          comments: []
+        }
+    }
+
+    render () {
+        return (
+            <Box>
+                工作台
+            </Box>
+        )
+    }
+}
+
+export default WorkBench
